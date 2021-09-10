5283f54e6bd218653ede4d392b8be97cd6b262ef
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-10 18:33:38 +0300

    2

--- hw-6-t2/src/App.js
+++ hw-6-t2/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: true,
+      title: "Текущее время",
+      date: new Date(),
+      counter: 0
+    }
+  
+
+  // setInterval(() => {
+  //   this.setState((state, props) => ({
+  //     date: new Date(),
+  //     counter: state.counter + 1.
+  //   }));
+  // }, 1000);
+} 
+
+componentDidMount() {
+  console.log("component did mount");
+
+   this.intervalFlag = setInterval(() => {
+    this.setState((state, props) => ({
+      date: new Date(),
+      counter: state.counter + 1.
+    }));
+  }, 1000);
+}
+
+componentDidUpdate() {
+  console.log("component did update");
+}
+
+componentWillUnmount() {
+  console.log("component will unmount");
+  clearInterval(this.intervalFlag);
+}
+
+handleClick = () => {
+ let show = !this.state.show;
+this.setState({show: show})
+} 
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{this.state.title}</p>
+      <p>{this.state.counter}</p>
+      <p>{this.state.date.toLocaleTimeString()}</p>
     </div>
   );
+}
 }
 
 export default App;
